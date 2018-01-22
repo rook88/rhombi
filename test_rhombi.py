85bd8c051592da62723085ba9368359e1f4969d8
rook <rook@OS.home>
2018-01-22 21:16:45 +0200

    3 parts

--- test_rhombi.py
+++ test_rhombi.py
@@ -5,17 +5,18 @@ import imageio
 
 import rhombi
 
-rhombi.imgWidth = 6400
-rhombi.imgHeight= 6400
-rhombi.sideLength = 240
-rhombi.backgroundColor = (255, 93, 15)
-rhombi.rhombusEdgeColor = (0, 0, 0)
-rhombi.rhombusEdgeThickness = 10
-
-frameCount = 20
-lineCount = 40
+rhombi.imgWidth = 1280 * 5
+rhombi.imgHeight= 960 * 5
+rhombi.sideLength = 20 * 5
+rhombusEdgeColorStart = np.array([15, 93, 255])
+rhombusEdgeColorEnd = np.array([255, 93, 15]) 
+rhombi.backgroundColor = (0, 0, 0)
+rhombi.rhombusEdgeThickness = 8
 
 
+lineCount = 70
+
+"""
 fs = [(lambda x : np.exp(angle * (x + 0.1 * n))) for n in range(frameCount)]
 fs = [rhombi.curry(z) for z in list(np.linspace(0, 1, frameCount + 1))][:-1]
 
@@ -25,16 +26,25 @@ ks = [range(frameCount) for n in range(frameCount)]
 ks = [list(np.linspace(1, n + 3, n + 3)) for n in range(frameCount)]
 fs = [rhombi.star(13) for n in range(frameCount)]
 
-ts = list(np.linspace(0, 1, frameCount))
-ks = [list(np.linspace(1, 21, 21)) for n in range(frameCount)]
+"""
 
-fs = [rhombi.star((np.sqrt(5) + 1) / 2 + (1 - t) / 4, 1) for t in ts]
+
+frameCount = 600
+ts = list(np.linspace(0, 1, frameCount + 1))[:-1]
+ks = [list(np.linspace(1, lineCount, lineCount)) for n in range(frameCount)]
+fs = [rhombi.star((np.sqrt(5) + 1) / 2 - (1 - t) / 2, 1) for t in ts]
 linesGroup = [rhombi.genLines(k, f) for k, f in zip(ks, fs)]
 
-fs = [rhombi.star((np.sqrt(5) + 1) / 2, 1 - t) for t in ts]
+frameCount = 300
+ts = list(np.linspace(0, 1, frameCount + 1))[:-1]
+ks = [list(np.linspace(1, lineCount, lineCount)) for n in range(frameCount)]
+fs = [rhombi.star((np.sqrt(5) + 1) / 2, (1 - t) * (1 - t)) for t in ts]
 linesGroup += [rhombi.genLines(k, f) for k, f in zip(ks, fs)]
 
-fs = [rhombi.star((np.sqrt(5) + 1) / 2 + t / 4, 0) for t in ts]
+frameCount = 600
+ts = list(np.linspace(0, 1, frameCount + 1))[:-1]
+ks = [list(np.linspace(1, lineCount, lineCount)) for n in range(frameCount)]
+fs = [rhombi.star((np.sqrt(5) + 1) / 2 - t / 2, 0) for t in ts]
 linesGroup += [rhombi.genLines(k, f) for k, f in zip(ks, fs)]
 
 
@@ -45,9 +55,11 @@ print [f(1) for f in fs]
 
 ims = []
 for i, lines in zip(range(len(linesGroup)), linesGroup):
-    print("frame {}/{} line[0] = {}".format(i + 1, len(linesGroup), lines[0]))
+    framePct = float(i) / len(linesGroup)
+    print("frame {}/{} framePct = {} line[0] = {}".format(i + 1, len(linesGroup), framePct, lines[0]))
+    rhombi.rhombusEdgeColor = rhombusEdgeColorStart * (1 - framePct) + rhombusEdgeColorEnd * framePct
     img = rhombi.drawImg(lines = lines)
-    img = cv2.resize(img, (640, 640))
+    img = cv2.resize(img, (1280, 960))
     if frameCount < 10:
         cv2.imshow('image',img)
         cv2.waitKey(0)
